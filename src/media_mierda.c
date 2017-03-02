42998028747397b01e22e1030d0ed4bbf87ecb43
ernesto.alvarado@oracle.com <ealvarad@slc02oyd.us.oracle.com>
2017-03-02 10:04:34 -0800

    anadia valida

--- src/media_mierda.c
+++ src/media_mierda.c
@@ -1095,6 +1095,260 @@ void avl_tree_borrar(avl_tree_t *tree, tipo_dato value) {
 
 #endif
 
+
+#if 1
+
+// XXX: https://www.tutorialspoint.com/data_structures_algorithms/linked_list_program_in_c.htm
+
+struct node {
+   int data;
+   int key;
+   struct node *next;
+};
+
+struct node *head = NULL;
+struct node *current = NULL;
+
+//display the list
+void printList() {
+   struct node *ptr = head;
+   caca_log_debug("imprimiendo lista");
+	
+   //start from the beginning
+   while(ptr != NULL) {
+      caca_log_debug("(%d,%d) ",ptr->key,ptr->data);
+      ptr = ptr->next;
+   }
+	
+}
+
+//insert link at the first location
+void insertFirst(int key, int data) {
+   //create a link
+   struct node *link = (struct node*) malloc(sizeof(struct node));
+	
+   link->key = key;
+   link->data = data;
+	
+   //point it to old first node
+   link->next = head;
+	
+   //point first to new first node
+   head = link;
+}
+
+//delete first item
+struct node* deleteFirst() {
+
+   //save reference to first link
+   struct node *tempLink = head;
+	
+   //mark next to first link as first 
+   head = head->next;
+	
+   //return the deleted link
+   return tempLink;
+}
+
+//is list empty
+bool isEmpty() {
+   return head == NULL;
+}
+
+int length() {
+   int length = 0;
+   struct node *current;
+	
+   for(current = head; current != NULL; current = current->next) {
+      length++;
+   }
+	
+   return length;
+}
+
+//find a link with given key
+struct node* find(int key) {
+
+   //start from the first link
+   struct node* current = head;
+
+   //if list is empty
+   if(head == NULL) {
+      return NULL;
+   }
+
+   //navigate through list
+   while(current->key != key) {
+	
+      //if it is last node
+      if(current->next == NULL) {
+         return NULL;
+      } else {
+         //go to next link
+         current = current->next;
+      }
+   }      
+	
+   //if data found, return the current Link
+   return current;
+}
+
+//delete a link with given key
+struct node* delete(int key) {
+
+   //start from the first link
+   struct node* current = head;
+   struct node* previous = NULL;
+	
+   //if list is empty
+   if(head == NULL) {
+      return NULL;
+   }
+
+   //navigate through list
+   while(current->key != key) {
+
+      //if it is last node
+      if(current->next == NULL) {
+         return NULL;
+      } else {
+         //store reference to current link
+         previous = current;
+         //move to next link
+         current = current->next;
+      }
+   }
+
+   //found a match, update the link
+   if(current == head) {
+      //change first to point to next link
+      head = head->next;
+   } else {
+      //bypass the current link
+      previous->next = current->next;
+   }    
+	
+   return current;
+}
+
+void sort() {
+
+   int i, j, k, tempKey, tempData;
+   struct node *current;
+   struct node *next;
+	
+   int size = length();
+   k = size ;
+	
+   for ( i = 0 ; i < size - 1 ; i++, k-- ) {
+      current = head;
+      next = head->next;
+		
+      for ( j = 1 ; j < k ; j++ ) {   
+		
+         if ( current->key > next->key ) {
+            tempData = current->key;
+            current->key = next->key;
+            next->key = tempData;
+
+            tempKey = current->data;
+            current->data = next->data;
+            next->data = tempKey;
+         }
+			
+         current = current->next;
+         next = next->next;
+      }
+   }   
+}
+
+void reverse(struct node** head_ref) {
+   struct node* prev   = NULL;
+   struct node* current = *head_ref;
+   struct node* next;
+	
+   while (current != NULL) {
+      next  = current->next;
+      current->next = prev;   
+      prev = current;
+      current = next;
+   }
+	
+   *head_ref = prev;
+}
+#if 0
+
+main() {
+   insertFirst(1,10);
+   insertFirst(2,20);
+   insertFirst(3,30);
+   insertFirst(4,1);
+   insertFirst(5,40);
+   insertFirst(6,56); 
+
+   printf("Original List: "); 
+	
+   //print list
+   printList();
+
+   while(!isEmpty()) {            
+      struct node *temp = deleteFirst();
+      printf("\nDeleted value:");
+      printf("(%d,%d) ",temp->key,temp->data);
+   }  
+	
+   printf("\nList after deleting all items: ");
+   printList();
+   insertFirst(1,10);
+   insertFirst(2,20);
+   insertFirst(3,30);
+   insertFirst(4,1);
+   insertFirst(5,40);
+   insertFirst(6,56);
+   
+   printf("\nRestored List: ");
+   printList();
+   printf("\n");  
+
+   struct node *foundLink = find(4);
+	
+   if(foundLink != NULL) {
+      printf("Element found: ");
+      printf("(%d,%d) ",foundLink->key,foundLink->data);
+      printf("\n");  
+   } else {
+      printf("Element not found.");
+   }
+
+   delete(4);
+   printf("List after deleting an item: ");
+   printList();
+   printf("\n");
+   foundLink = find(4);
+	
+   if(foundLink != NULL) {
+      printf("Element found: ");
+      printf("(%d,%d) ",foundLink->key,foundLink->data);
+      printf("\n");
+   } else {
+      printf("Element not found.");
+   }
+	
+   printf("\n");
+   sort();
+	
+   printf("List after sorting the data: ");
+   printList();
+	
+   reverse(&head);
+   printf("\nList after reversing the data: ");
+   printList();
+}
+#endif
+
+#endif
+
+
 void caca_comun_current_utc_time(struct timespec *ts) {
 
 #ifdef __MACH__
@@ -1459,6 +1713,75 @@ void media_mierda_main() {
 		double resul_bueno = (long long) resul;
 		caca_log_debug("el resu bueno %f", resul_bueno);
 		resul_bueno /= 2;
+
+
+#ifdef CACA_COMUN_VALIDA
+	bool izo=falso;
+	struct node* actual=NULL;
+	if(ope=='a')
+	{
+		insertFirst(num_actual,i);
+		izo=verdadero;
+	}
+	else
+	{
+		if(head && head->next && find(num_actual))
+		{
+			delete(num_actual);
+			izo=verdadero;
+		}
+	}
+	sort();
+	printList();
+	natural tam_caca=length();
+
+	natural mitad_caca=tam_caca>>1;
+	natural conta_caca=0;
+	int mediano=0;
+	int mediano_par=0;
+	tipo_dato puta=0;
+
+	if(izo)
+	{
+		if(tam_caca==1)
+		{
+			puta=(tipo_dato)head->key *2;
+		}
+		else
+		{
+			actual = head;
+			while(conta_caca<mitad_caca && actual)
+			{
+				actual=actual->next;
+				conta_caca++;
+			}
+			mediano=actual->key;
+			if(!(tam_caca%2))
+			{
+				actual = head;
+				conta_caca=0;
+				while(conta_caca<mitad_caca-1 && actual)
+				{
+					actual=actual->next;
+					conta_caca++;
+				}
+				mediano_par=actual->key;
+				puta=(tipo_dato)mediano+(tipo_dato)mediano_par;
+			}
+			else
+			{
+				puta=(tipo_dato)mediano*2;
+			}
+		}
+	}
+	else
+	{
+		puta=MEDIA_MIERDA_VALOR_INVALIDO;
+	}
+	assert_timeout(puta==resul);
+#endif
+
+
 		if (resul != MEDIA_MIERDA_VALOR_INVALIDO) {
 			if (resul % 2) {
 				printf("%.1f\n", resul_bueno);
